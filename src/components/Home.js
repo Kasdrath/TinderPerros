6160baea38e826ecebccb002ce4a63c4b512a8de
Martin Araneda <martix.araneda@gmail.com>
2022-11-10 23:29:07 -0300

    Adicion de icons button

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,15 @@ import {
   Container,
   Grid,
   Typography,
+  IconButton,
 } from "@mui/material";
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import axios from "axios";
 
 const Home = () => {
 
-  const [dog,setDog] = useState("");
+  const [dog, setDog] = useState("");
   const [nombrePerro, setNombre] = useState("");
   const [perroarrepentido, setPerroArrepentido] = useState("");
   const [perrorechazo, setPerroRechazo] = useState([]);
@@ -19,7 +22,7 @@ const Home = () => {
   const [disable, setDisable] = useState("");
   const cargarPerro = () => {
     console.log(dog);
-     axios.get("https://dog.ceo/api/breeds/image/random").then(
+    axios.get("https://dog.ceo/api/breeds/image/random").then(
       (response) => {
         setDog(response.data.message);
         setNombre(stringGen());
@@ -33,53 +36,51 @@ const Home = () => {
 
   useEffect(() => {
     cargarPerro();
-     // eslint-disable-next-line react-hooks/exhaustive-deps
-}, []);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-const moverPerroIzquierda =() => {
-  setDisable(true);
-  setPerroRechazo(previousState => [...previousState, dog]);
-  setPerroArrepentido("izquierda");
-  cargarPerro();
-}
-const moverPerroDerecha =() => {
-  setDisable(true);
-  setPerroAcepto(previousState => [...previousState, dog]);
-  setPerroArrepentido("derecha");
-  cargarPerro();
-}
-const arrepentirPerro =() => {
-  setDisable(true);
-  if (perroarrepentido === "izquierda")
-  {
-    setPerroAcepto(previousState => [...previousState, perrorechazo[perrorechazo.length-1]]);
-    setPerroRechazo(perrorechazo.slice(0, -1));
-    setPerroArrepentido("derecha");
-    setDisable(false);
-  }
-  else if(perroarrepentido === "derecha")
-  {
-    setPerroRechazo(previousState => [...previousState, perroacepto[perroacepto.length-1]]);
-    setPerroAcepto(perroacepto.slice(0, -1));
+  const moverPerroIzquierda = () => {
+    setDisable(true);
+    setPerroRechazo(previousState => [...previousState, dog]);
     setPerroArrepentido("izquierda");
-    setDisable(false);
+    cargarPerro();
   }
-}
-
-function stringGen(len = 6) {
-  let alphanumeric =
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-  let code = new Array();
-
-  for(let i=0; i<len; i++){
-    let index = Math.floor(Math.random() * alphanumeric.length);
-    code.push(alphanumeric.charAt(index));
+  const moverPerroDerecha = () => {
+    setDisable(true);
+    setPerroAcepto(previousState => [...previousState, dog]);
+    setPerroArrepentido("derecha");
+    cargarPerro();
+  }
+  const arrepentirPerro = () => {
+    setDisable(true);
+    if (perroarrepentido === "izquierda") {
+      setPerroAcepto(previousState => [...previousState, perrorechazo[perrorechazo.length - 1]]);
+      setPerroRechazo(perrorechazo.slice(0, -1));
+      setPerroArrepentido("derecha");
+      setDisable(false);
+    }
+    else if (perroarrepentido === "derecha") {
+      setPerroRechazo(previousState => [...previousState, perroacepto[perroacepto.length - 1]]);
+      setPerroAcepto(perroacepto.slice(0, -1));
+      setPerroArrepentido("izquierda");
+      setDisable(false);
+    }
   }
 
-  code = code.join("")
-  return code;
-}
+  function stringGen(len = 6) {
+    let alphanumeric =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    let code = new Array();
+
+    for (let i = 0; i < len; i++) {
+      let index = Math.floor(Math.random() * alphanumeric.length);
+      code.push(alphanumeric.charAt(index));
+    }
+
+    code = code.join("")
+    return code;
+  }
 
   return (
     <Container fixed sx={{ height: 1 }}>
@@ -97,28 +98,27 @@ function stringGen(len = 6) {
             height: 400,
             overflow: "hidden",
             overflowY: "scroll",
-          alignItems:"center",
-          justifyContent:"center"
+            alignItems: "center",
+            justifyContent: "center"
           }}
         >
           <Grid item xs={4}
-          sx={{
-            mt: 2,
-            height: 'auto',
-            overflow: "hidden",
-            overflowY: "scroll",
-            alignItems:"center",
-            justifyContent:"center"
-          }}>
-            <Typography style={{fontSize: 14}} backgroundColor="white" borderColor="grey" border={2} color="black" textAlign="center">Perros Rechazados</Typography>
-            {perrorechazo.map(function (perro, index)
-            {
-              return[
+            sx={{
+              mt: 2,
+              height: 'auto',
+              overflow: "hidden",
+              overflowY: "scroll",
+              alignItems: "center",
+              justifyContent: "center"
+            }}>
+            <Typography style={{ fontSize: 14 }} backgroundColor="white" borderColor="grey" border={2} color="black" textAlign="center">Perros Rechazados</Typography>
+            {perrorechazo.map(function (perro, index) {
+              return [
                 <img src={perro}
-                width='100' 
-                height='100'
-                alt="perros rechazados"/>,
-                <br/>
+                  width='100'
+                  height='100'
+                  alt="perros rechazados" />,
+                <br />
               ];
             })
             }
@@ -126,43 +126,54 @@ function stringGen(len = 6) {
 
 
           <Grid item xs={4}
-          sx={{
-            mt: 2,
-            height: '400',
-            overflow: "hidden",
-            overflowY: "scroll"
-          }}>
+            sx={{
+              mt: 2,
+              height: '400',
+              overflow: "hidden",
+              overflowY: "scroll"
+            }}>
             <Card>
-              {disable && "cargando"}
+              {disable && "Cargando"}
               <CardContent>
-                <Typography style={{fontSize: 14}} color="black" textAlign="center">
+                <Typography style={{ fontSize: 14 }} color="black" textAlign="center">
                   {nombrePerro}
                 </Typography>
-                {<img src={dog} width='200' height='200' alt="perros postulantes"/>}
-                
+                {<img src={dog} width='200' height='200' alt="perros postulantes" />}
+
               </CardContent>
             </Card>
-              {<button onClick={moverPerroIzquierda} disabled={disable}>  Rechazar Perro </button>}
-              {<button onClick={moverPerroDerecha} disabled={disable}>  Aceptar Perro </button>}
-              {<button onClick={arrepentirPerro} disabled={disable}>  !Me arrepentí de mi última elección! </button>}
+            <IconButton
+              onClick={moverPerroIzquierda}
+              disabled={disable}
+            >
+              <ThumbDownIcon />
+            </IconButton>
+            <IconButton
+              onClick={moverPerroDerecha}
+              disabled={disable}
+            >
+              <ThumbUpIcon />
+            </IconButton>
+            {<button onClick={moverPerroIzquierda} disabled={disable}>  Rechazar Perro </button>}
+            {<button onClick={moverPerroDerecha} disabled={disable}>  Aceptar Perro </button>}
+            {<button onClick={arrepentirPerro} disabled={disable}>  !Me arrepentí de mi última elección! </button>}
           </Grid>
 
           <Grid item xs={4}
-          sx={{
-            mt: 2,
-            height: 'auto',
-            overflow: "hidden",
-            overflowY: "scroll"
-          }}>
-            <Typography style={{fontSize: 14}} backgroundColor="white" borderColor="grey" border={2} color="black" textAlign="center">Perros Aceptados</Typography>
-            {perroacepto.map(function (perro, index)
-            {
-              return[
+            sx={{
+              mt: 2,
+              height: 'auto',
+              overflow: "hidden",
+              overflowY: "scroll"
+            }}>
+            <Typography style={{ fontSize: 14 }} backgroundColor="white" borderColor="grey" border={2} color="black" textAlign="center">Perros Aceptados</Typography>
+            {perroacepto.map(function (perro, index) {
+              return [
                 <img src={perro}
-                width='100' 
-                height='100'
-                alt="perros aceptados"/>,
-                <br/>
+                  width='100'
+                  height='100'
+                  alt="perros aceptados" />,
+                <br />
               ];
             })
             }
@@ -171,8 +182,8 @@ function stringGen(len = 6) {
 
         </Grid>
       </Box>
-    </Container>
-    
+    </Container >
+
   );
 };
 export default Home;
