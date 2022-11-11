cb8b2afe106e263df748c2afb96ad4656ca2dc42
Martin Araneda <martix.araneda@gmail.com>
2022-11-10 22:34:53 -0300

    commit inicial

---
+++ src/components/PerroCandidato.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { IconButton, Card, CardActions, CardContent, } from "@mui/material";
+import ThumbDownIcon from '@mui/icons-material/ThumbDown';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+
+
+
+export default Perro;
